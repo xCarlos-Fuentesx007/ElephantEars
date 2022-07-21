572a814b1e185411a72cbcd9e595b142c9ad0bad
Ethan <ethankaplan97@yahoo.com>
2022-07-21 15:30:31 -0400

    Modified index.js to comply with Node 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import AuthContextProvider from "./context/auth-context";
 
-ReactDOM.render(
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <AuthContextProvider>
     <App />
-  </AuthContextProvider>,
-  document.getElementById("root")
+  </AuthContextProvider>
 );
