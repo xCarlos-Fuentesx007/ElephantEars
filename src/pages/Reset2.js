9777a4f76987d1290c75558e793b849fb8ec641a
Ethan <ethankaplan97@yahoo.com>
2022-07-23 18:16:44 -0400

    Added error code logic to Reset2.js, but need database access to finish validateCode()

--- src/pages/Reset2.js
+++ src/pages/Reset2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Paper,
@@ -20,6 +20,39 @@ const DisplayErr = (errorCode) => {
       return <Alert severity="error">An unknown error has occurred</Alert>;
   }
 };
+
+// Checks if code is valid and returns an error from DisplayErr if it isn't
+function validateCode(code) {
+  // get code from database(?)
+  // assign to variable dbCode
+  // compare with code
+    // if equal, reset password and return nothing or DisplayErr(0)
+  // if not equal, return DisplayErr(1)
+  // console.log('from code')
+  return (code === '' || code === undefined) ? DisplayErr(0) : DisplayErr(1);
+}
+
+function Confirm() {
+  const [code, setCode] = useState();
+  const updateCode = () => setCode(() => (document.getElementById('code') === null) ? 0 : document.getElementById("code").value);
+
+  const [error, setError] = useState();
+  const makeError = () => setError(() => validateCode(code));
+
+  useEffect(() => {
+    makeError();
+    console.log(code);
+  }, [code]);
+
+  return (
+    <Container>
+      <Button type="submit" onClick={() => updateCode()} fullWidth variant="contained" sx={{ my: 2 }}>
+        Confirm
+      </Button>
+      {error}
+    </Container>
+  )
+}
 
 const Reset2 = () => {
   return (
@@ -63,11 +96,12 @@ const Reset2 = () => {
             />
           </Container>
 
-          <Container>
+          {/* <Container>
             <Button type="submit" fullWidth variant="contained" sx={{ my: 2 }}>
               Confirm
             </Button>
-          </Container>
+          </Container> */}
+          {Confirm()}
 
           <Typography
             variant="body2"
@@ -82,7 +116,7 @@ const Reset2 = () => {
             to send another.
           </Typography>
 
-          {DisplayErr(1)}
+          {/* {DisplayErr(1)} */}
         </Paper>
       </Container>
     </>
